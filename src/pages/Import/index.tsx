487fb465933141288e1d81e1cd115ef194f49989
Alex Botelho <alexbotelho1@hotmail.com>
2020-07-22 09:55:50 -0300

    Desafio 7 finalizado

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -25,17 +25,29 @@ const Import: React.FC = () => {
   async function handleUpload(): Promise<void> {
     const data = new FormData();
 
-    // TODO
+    if (!uploadedFiles.length) return;
+
+    const file = uploadedFiles[0];
+
+    data.append('file', file.file, file.name);
 
     try {
       await api.post('/transactions/import', data);
+
+      history.push('/');
     } catch (err) {
       console.log(err.response.error);
     }
   }
 
-  function submitFile(files: File): void {
-    setUploadedFiles(files)
+  function submitFile(files: File[]): void {
+    const uploadFiles = files.map(file => ({
+      file,
+      name: file.name,
+      readableSize: filesize(file.size),
+    }));
+
+    setUploadedFiles(uploadFiles);
   }
 
   return (
